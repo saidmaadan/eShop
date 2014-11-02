f446e6e807611f229c466bcce509b2da5997f7f4
Said Maadan <saidmaadan@users.noreply.github.com>
2014-11-02 12:52:37 -0600

    Create store catalog page

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -39,19 +39,20 @@ class ProductTest < ActiveSupport::TestCase
   	bad = %w{ fred.doc fred.gif/more fred.gif.more }
   	ok.each do |name|
   		assert new_product(name).valid?, "#{name} should be valid"
+  	end
   	bad.each do |name|
   		assert new_product(name).invalid?, "#{name} should't be valid"
   	end
   end
 
   test "product is not valid without a unique title" do
-  	product = Product.new(title: product(:ruby).title, description: "product title", price: 1, image_url: "ghost.jpeg")
+  	product = Product.new(title: products(:ruby).title, description: "product title", price: 1, image_url: "ghost.jpeg")
   	assert product.invalid?
   	assert_equal ["has already been taken"], product.errors[:title]
   end
 
   test "product is not valid without a unique title - i18n" do
-  	product = Product.new(title: product(:ruby).title, description: "product title", price: 1, image_url: "ghost.jpeg")
+  	product = Product.new(title: products(:ruby).title, description: "product title", price: 1, image_url: "ghost.jpeg")
   	assert product.invalid?
   	assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
